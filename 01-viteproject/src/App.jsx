c0e5d68683f7a2f6323ab616c8218584173a84f1
Aswini7978 <amk08283@.com>
2026-09-18 21:15:12 +0530

    Code

--- 01-viteproject/src/App.jsx
+++ 01-viteproject/src/App.jsx
@@ -3,13 +3,17 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Card from './component/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-500 p-4 rounded-m'>Aswini</h1>
+      {/* <h1 className='bg-green-500 p-4 rounded-m'>Aswini</h1> */}
+      <Card username='Aswini'/>
+      {/* <Card username='hello'/> */}
+      
     </>
   )
 }
