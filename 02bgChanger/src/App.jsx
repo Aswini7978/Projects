12923049ddf190f132a8d849c1c6557980c262fe
Aswini7978 <amk08283@.com>
2026-09-21 18:55:32 +0530

    code

--- 02bgChanger/src/App.jsx
+++ 02bgChanger/src/App.jsx
@@ -5,116 +5,33 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div className='flex justify-center text-black items-end min-h-screen gap-4'
+      style={{backgroundColor:color}}
+      > 
+        <button className='rounded-xl p-4 bg-red-400 px-5 py-5 m-5 shadow-2xl'
+        onClick={()=>{setColor("red")}}
+        >Red</button>
+        <button className='rounded-xl p-4 bg-green-500 m-5 px-5 py-5 '
+        onClick={()=>{setColor("green")}}
+        >green</button>
+        <button className='rounded-xl p-4 bg-yellow-500 m-5 px-5 py-5'
+        onClick={()=>{setColor("yellow")}}
+        >yellow</button>
+        <button className='rounded-xl p-4 bg-blue-500 m-5 px-5 py-5'
+        onClick={()=>{setColor("blue")}}
+        >blue</button>
+        <button className='rounded-xl p-4 bg-pink-500 m-5 px-5 py-5'
+        onClick={()=>{setColor("pink")}}
+        >pink</button>
+        <button className='rounded-xl p-4 bg-violet-500 m-5 px-5 py-5'
+        onClick={()=>{setColor("violet")}}
+        >violet</button>
+        </div>         
+         
     </>
   )
 }
